b5a050aa7e9a0882df554b08ef00e9f379d3f7f1
George <45212822+antoniougit@users.noreply.github.com>
2023-12-06 23:03:40 +0200

    Create elements.js

---
+++ elements.js
@@ -0,0 +1,9 @@
+export const ELEMENT_SEARCH_BUTTON = document.querySelector('button');
+export const ELEMENT_SEARCHED_CITY = document.getElementById('city');
+export const ELEMENT_LOADING_TEXT = document.getElementById('load');
+export const ELEMENT_WEATHER_BOX = document.getElementById('weather');
+export const ELEMENT_WEATHER_CITY = document.getElementById('weatherCity');
+export const ELEMENT_WEATHER_DESCRIPTION =
+    document.getElementById('weatherDescription');
+export const ELEMENT_WEATHER_TEMPERATURE =
+    document.getElementById('weatherTemperature');
