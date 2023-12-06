07c22420de46249630a1ded1161aada6d3f0dc54
George <45212822+antoniougit@users.noreply.github.com>
2023-12-06 23:14:56 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,5 +3,8 @@ import * as ELEMENTS from 'elements.js';
 ELEMENTS.ELEMENT_SEARCH_BUTTON.addEventListener('click', searchWeather);
 
 function searchWeather() {
-    
+    const CITY_NAME = ELEMENTS.ELEMENT_SEARCHED_CITY.value.trim();
+    if (CITY_NAME.length == 0) {
+        return alert('Please enter a city name!');
+    }
 }
