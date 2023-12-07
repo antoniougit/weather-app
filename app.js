9ebe1d14a8230d3df6958c61261ef7a75f70df5b
George <45212822+antoniougit@users.noreply.github.com>
2023-12-07 23:08:33 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import * as ELEMENTS from 'elements';
 import { Http } from 'http';
+import { WeatherData, WEATHER_PROXY_HANDLER } from 'weather-data';
 
 const API_KEY = 'c5941ee1946f17708a86adfbfb19dfcc';
 
@@ -10,11 +11,19 @@ function searchWeather() {
     if (CITY_NAME.length == 0) {
         return alert('Please enter a city name!');
     }
-    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${CITY_NAME}&units=metric&appid=${API_KEY}`;
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${CITY_NAME}&appid=${API_KEY}`;
 
     Http.fetchData(URL)
         .then((responseData) => {
-            
+            const WEATHER_DATA = new WeatherData(
+                CITY_NAME,
+                responseData.weather[0].description.toUpperCase()
+            );
+            const WEATHER_PROXY = new Proxy(
+                WEATHER_DATA,
+                WEATHER_PROXY_HANDLER
+            );
+            WEATHER_PROXY.temperature = responseData.main.temp;
         })
         .catch((error) => alert(error));
 }
