10cc048ada8342bc9466a103c6fad1bf09b27674
George <45212822+antoniougit@users.noreply.github.com>
2023-12-07 23:11:46 +0200

    Update app.js

--- app.js
+++ app.js
@@ -24,6 +24,15 @@ function searchWeather() {
                 WEATHER_PROXY_HANDLER
             );
             WEATHER_PROXY.temperature = responseData.main.temp;
+            updateWeather(WEATHER_PROXY);
         })
         .catch((error) => alert(error));
+}
+
+function updateWeather(weatherData) {
+    ELEMENTS.ELEMENT_WEATHER_CITY.textContent = weatherData.cityName;
+    ELEMENTS.ELEMENT_WEATHER_DESCRIPTION.textContent = weatherData.description;
+    ELEMENTS.ELEMENT_WEATHER_TEMPERATURE.textContent = weatherData.temperature;
+
+    ELEMENTS.ELEMENT_WEATHER_BOX.style.display = 'block';
 }
