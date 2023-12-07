b46bcfbd3ed79d9de26748dd035831db1d762b13
George <45212822+antoniougit@users.noreply.github.com>
2023-12-07 22:58:23 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-import * as ELEMENTS from 'elements.js';
+import * as ELEMENTS from 'elements';
+import { Http } from 'http';
+
+const API_KEY = 'c5941ee1946f17708a86adfbfb19dfcc';
 
 ELEMENTS.ELEMENT_SEARCH_BUTTON.addEventListener('click', searchWeather);
 
@@ -7,4 +10,11 @@ function searchWeather() {
     if (CITY_NAME.length == 0) {
         return alert('Please enter a city name!');
     }
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${CITY_NAME}&units=metric&appid=${API_KEY}`;
+
+    Http.fetchData(URL)
+        .then((responseData) => {
+            
+        })
+        .catch((error) => alert(error));
 }
